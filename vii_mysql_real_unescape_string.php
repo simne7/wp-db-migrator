8a641ed253e8e6eeb926273c656a32c78083a0e9
Peter-Benedikt von Niebelschütz <mail@vonniebelschuetz.de>
2015-02-19 11:47:24 +0100

    unescape improvement

--- vii_mysql_real_unescape_string.php
+++ vii_mysql_real_unescape_string.php
@@ -4,22 +4,30 @@ function vii_mysql_real_unescape_string($str){
     
     $search = array(
         '\0',
-        '\n',
-        '\r',
-        '\\\\',// yeah, here we have
         '\\\'',// all the variations of
         '\\"',// php/regex backslash escaping
-        '\Z'
+	'\b', // backspace character
+        '\n',
+        '\r',
+	'\t', // tabulator
+        '\Z',
+        '\\\\',// yeah, here we have
+	'\%',
+	'\_',
     ); 
         
     $replace = array(
         chr(0),// \0,
-        chr(10),// \n,
-        chr(13),// \r,
-        chr(92),// \,
         chr(39),// ',
         chr(34),// ",
+	chr(8), // \b
+        chr(10),// \n,
+        chr(13),// \r,
+	chr(9), // \t
         chr(26),// \Z,
+        chr(92),// \,
+	chr(37), // %
+	chr(95), // _
     );   
     
     return str_replace($search, $replace, $str);
