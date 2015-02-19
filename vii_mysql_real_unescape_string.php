9879c72e57aea05c2e4907f5af6ea1b8de39b1fc
Peter-Benedikt von Niebelschütz <mail@vonniebelschuetz.de>
2015-02-19 12:41:10 +0100

    reverted unescape changes

--- vii_mysql_real_unescape_string.php
+++ vii_mysql_real_unescape_string.php
@@ -6,28 +6,20 @@ function vii_mysql_real_unescape_string($str){
         '\0',
         '\\\'',// all the variations of
         '\\"',// php/regex backslash escaping
-	'\b', // backspace character
         '\n',
         '\r',
-	'\t', // tabulator
         '\Z',
         '\\\\',// yeah, here we have
-	'\%',
-	'\_',
     ); 
         
     $replace = array(
         chr(0),// \0,
         chr(39),// ',
         chr(34),// ",
-	chr(8), // \b
         chr(10),// \n,
         chr(13),// \r,
-	chr(9), // \t
         chr(26),// \Z,
         chr(92),// \,
-	chr(37), // %
-	chr(95), // _
     );   
     
     return str_replace($search, $replace, $str);
